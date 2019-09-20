3456828119650fb4515cb0b16fd6d51e416dff76
Nancy Nguyen <41247637+nancyast@users.noreply.github.com>
2019-09-20 10:44:04 +0700

    Add topic: Big O Notation

---
+++ src/1_Big_O_Notation.ts
@@ -0,0 +1,11 @@
+// TIME COMPLEXITY
+// 1. Arithmetic operations are constant.
+// 2. Varriable assignment is contant.
+// 3. Accessing elements in an array(by index) or object(by key) is constant.
+// 4. In a loop, the complexity is the length of the loop times the complexity of whatever happens inside of the loop.
+
+// SPACE COMPLEXITY
+// Auxiliary space complexity - space required by the algorithm, not including space taken up by the inputs.
+// 1. Most primitives (boolean, numbers, undefined, null) ara constant space. O(1).
+// 2. Strings require O(n) space (where n is the string length).
+// 3. Reference types are generally O(n), where n is the length(for array) or the number of keys(for objects).
---
+++ src/2_Problem_Solving_Approach.ts
@@ -0,0 +1,29 @@
+// Step 1. Understand the problem:
+//      <1> Can I restate the problem in my own word.
+//      <2> What are the inputs that go into the problem.
+//      <3> What are thr outputs that should come from the solution to the problem?
+//      <4> Can the outputs be determined from the inputs?
+//      <5> How should I label the important pieces of data that are a part of the problem?
+// Step 2. Concrete examples:
+//      <1> Start with simple examples
+//      <2> Progress to more complex examples
+//      <3> Explore examples with empty inputs
+//      <4> Explore examples with invalid inputs
+// Step 3. Break it down
+//      <1>
+//      <2>
+//      <3>
+//      <4>
+// Step 4. Solve Or Simplify
+//      <1> Find the core difficulty in what you're trying to do
+//      <2> Temporarily ignore th at difficulty
+//      <3> Write a simplified solution
+//      <4> Then incorporate that difficulty back in
+// Step 5. Look back and refactor
+// * Refactoring questions:
+//      <1> Can you check the result?
+//      <2> Can you derive the result differently?
+//      <3> Can you inderstand it at a glance?
+//      <4> Can you use the result or method for some other problem?
+//      <5> Can you think of other ways to refactor?
+//      <6> How have other people solved this problem?
